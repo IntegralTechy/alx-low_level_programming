155e528b28099f486170f8020226f983a06826ee
IntegralTechy <egborichard1946@gmail.com>
2022-12-09 20:26:20 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase followed by new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	while (alpha == 'a' && alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
