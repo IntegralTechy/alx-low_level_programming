94a074e4598bc4cc0d0167cd643bd95857972ab2
IntegralTechy <egborichard1946@gmail.com>
2022-12-13 20:44:53 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char letter;
 
-	while (i <= 9)
+	while (i++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
 	}
-		i++
+	
 }
